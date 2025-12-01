7e8ae068a0a89a315cf7dfc0a6af07c97b068f7f
theblackpekka <azkaawliya06@gmail.com>
2025-12-01 16:37:13 +0700

    menghapus beberapa import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:oliminate_mobile/features/main-page/main_page.dart';
-import 'package:oliminate_mobile/features/user-profile/edit_profile.dart';
 import 'package:oliminate_mobile/features/user-profile/login.dart';
-import 'package:oliminate_mobile/features/user-profile/main_profile.dart';
-import 'package:oliminate_mobile/features/user-profile/register.dart';
 
 void main() {
   runApp(const MyApp());
